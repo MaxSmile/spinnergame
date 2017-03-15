963759cd03463b825b6a9c440c0ff97a70c278b0
Volodymyr Kusenko <volodymyr.kusenko@gmail.com>
2017-03-15 20:04:08 +0200

    Implement new design

--- Spinner/app/src/main/java/com/vasilkoff/spinnergame/MainActivity.java
+++ Spinner/app/src/main/java/com/vasilkoff/spinnergame/MainActivity.java
@@ -1,10 +1,12 @@
 package com.vasilkoff.spinnergame;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,9 +14,15 @@ import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
+    private ProgressBar progressBar;
+    private int count = 0;
+    private boolean flag;
+    private Handler handler;
+    private ImageView powerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,23 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        progressBar = (ProgressBar)findViewById(R.id.powerBar);
+        progressBar.setMax(100);
+        progressBar.setProgress(0);
+        progressBar.setProgressDrawable(ContextCompat.getDrawable(this, R.drawable.progress));
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message message) {
+                progressBar.setProgress(message.arg1);
+                return false;
+            }
+        });
+
+        powerButton = (ImageView)findViewById(R.id.powerButton);
+        powerButton.setOnTouchListener(new PowerTouchListener());
+
         initSpinner();
     }
 
@@ -38,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
     String prizeText = "N/A";
 
     private void initSpinner() {
-        pointerImageView = (ImageView)findViewById(R.id.imageView);
+        pointerImageView = (ImageView)findViewById(R.id.imageWheel);
         buttonImage = (ImageView)findViewById(R.id.buttonImage);
         infoText = (TextView) findViewById(R.id.info);
         buttonImage.setOnClickListener(new View.OnClickListener() {
@@ -47,12 +72,27 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
                 StartSpinner();
             }
         });
-        mSpinRevolutions = 3600;
+
+
+
     }
 
 
     public void StartSpinner() {
-        mSpinDuration = (long)(2000 + Math.random() * 3000);//random: 2000-5000
+        //mSpinDuration = (long)(2000 + Math.random() * 3000);//random: 2000-5000
+        mSpinRevolutions = 3600;
+        mSpinDuration = 5000;
+
+        if (count >= 30) {
+            mSpinDuration = 10000;
+            mSpinRevolutions = 3600 * 2;
+        }
+
+        if (count >= 60) {
+            mSpinDuration = 15000;
+            mSpinRevolutions = 3600 * 3;
+        }
+
 
         // Final point of rotation defined right here:
         int end = (int)Math.floor(Math.random() * 360);//random: 0-360
@@ -109,5 +149,42 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
     @Override
     public void onAnimationRepeat(Animation animation) {
 
+    }
+
+    private class PowerTouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    flag = true;
+                    count = 0;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (flag) {
+                                count++;
+                                if (count == 100)
+                                    flag = false;
+
+                                Message msg = new Message();
+                                msg.arg1 = count;
+                                handler.sendMessage(msg);
+                                try {
+                                    Thread.sleep(10);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }).start();
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    flag = false;
+                    StartSpinner();
+                    return true;
+            }
+            return false;
+        }
     }
 }
